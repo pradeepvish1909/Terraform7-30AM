f9f340d8e7ff4ba48c729af935272888826eacd4
Vishwakarma <pradeep.vishwakarma@capgemini.com>
2026-04-20 12:45:45 +0530

    Day 2 changes added

---
+++ Day-2-All-Config-Files/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "Passing values to AMI_ID"
+  default = ""
+  type = string
+}
+
+variable "instance_type" {
+  description = "Passing values to Instance_Type"
+  default = ""
+  type = string
+}
