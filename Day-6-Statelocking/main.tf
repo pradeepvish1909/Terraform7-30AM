901927257f8feeebff5fb22dee743a090e2a28ca
Vishwakarma <pradeep.vishwakarma@capgemini.com>
2026-04-22 14:08:59 +0530

    Day 6 changes added

--- Day-6-Statelocking/main.tf
+++ Day-6-Statelocking/main.tf
@@ -7,8 +7,16 @@ resource "aws_vpc" "name" {
 
 resource "aws_subnet" "name" {
   vpc_id = aws_vpc.name.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/25"
   tags = {
     Name = "prod-subnet"
+  }
+}
+
+resource "aws_subnet" "name-1" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.0.128/25"
+  tags = {
+    Name = "dev-subnet"
   }
 }
