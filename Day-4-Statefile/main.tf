d5d15e7daca014d3390b0f2c63bad72d19efe28f
Vishwakarma <pradeep.vishwakarma@capgemini.com>
2026-04-22 08:33:20 +0530

    Adding Day 4 changes

---
+++ Day-4-Statefile/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_instance" "name" {
+  ami = "ami-098e39bafa7e7303d"
+  instance_type = "t2.micro"
+  iam_instance_profile = "dev-instance-profile"
+  tags = {
+    Name = "TEST-instance"
+  }
+}
